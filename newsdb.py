e194a506f86a0cc61c86f870607f830b1392f589
doctor_entropy <gauthamv93@yahoo.com>
2019-04-22 19:55:46 -0600

    Initial commit with starter code

---
+++ newsdb.py
@@ -0,0 +1,10 @@
+import psycopg2
+
+class NewsDatabase:
+
+    def __init__(self):
+        self.db = psycopg2.connect(database='news')
+        self.conn = self.db.cursor()
+
+if __name__=="__main__":
+    news_database = NewsDatabase()
