8442d25488b18cf2ac982562436cae187165b5a8
doctor_entropy <gauthamv93@yahoo.com>
2019-04-23 19:51:54 -0600

    Initial SQL query to get popular articles

--- newsdb.py
+++ newsdb.py
@@ -6,5 +6,15 @@ class NewsDatabase:
         self.db = psycopg2.connect(database='news')
         self.conn = self.db.cursor()
 
+    def get_popular_articles(self, limit_number=3):
+        self.conn.execute("\
+            SELECT right(path, -9) as log_slug, count(*) \
+            FROM log WHERE status = '200 OK' and path != '/' \
+            GROUP BY log_slug ORDER BY count DESC")
+
+        posts = self.conn.fetchall()
+        print(posts)
+
 if __name__ == "__main__":
     news_database = NewsDatabase()
+    news_database.get_popular_articles()
